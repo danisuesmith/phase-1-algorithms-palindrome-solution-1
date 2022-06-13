382a871a54c5d1a0c0115e37ea87d98ae5dc8d1a
Dani <danisuesmith@gmail.com>
2022-06-13 10:49:11 -0700

    commit tree 3ead21ee63086435d9a88bcfe48222f0e7d6c112

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  let l = str.length;
+  for (let i = 0; i < l / 2; i++) {
+    if (str[i] !== str[l - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
@@ -13,8 +19,34 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("cat"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
